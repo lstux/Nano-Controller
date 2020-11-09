e19bd30e20312f50d5443dee8a9454374696f7cc
lstux <33700752+lstux@users.noreply.github.com>
2020-11-09 11:48:13 +0100

    Create NanoController.h

---
+++ NanoController.h
@@ -0,0 +1,30 @@
+#ifndef _NanoController_h_
+#define _NanoController_h_
+
+// Pins affectation
+#define JOY1_X   
+#define JOY1_Y   
+#define JOY1_BTN 
+#define JOY2_X   
+#define JOY2_Y   
+#define JOY2_BTN 
+#define BTN_A 
+#define BTN_B 
+#define BTN_X 
+#define BTN_Y 
+#define BTN_SELECT
+#define BTN_START
+
+/* NRF24L01 pin (SPI)
+/
+/
+/
+/
+/*
+
+/* LCD display pins (I2C)
+/
+/
+/*
+
+#endif
